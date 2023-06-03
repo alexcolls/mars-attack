64f1198b3a717fe1a052a7e084783c62fd28f159
quantium-rock <alexcollsoutumuro@gmail.com>
2023-06-03 16:49:55 +0200

    add SocialFisher lib running v1

---
+++ libs/social-fishing/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+$ cd src
+echo "Insert your username: $1"
+echo "Insert your password: $2"
+$ python3 main.py username password
+$ ./myscript hello world
+First arg: username
+Second arg: password
