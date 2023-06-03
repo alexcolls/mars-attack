3e0ee98b06c794bcf5ecc67eef90d7a9b549a749
quantium-rock <alexcollsoutumuro@gmail.com>
2023-06-03 14:57:39 +0200

    update spider v2 and v3

---
+++ code/web/spider/spider-v2.py
@@ -0,0 +1,33 @@
+import urllib.request as urllib2
+from bs4 import *
+from urllib.parse  import urljoin
+
+
+def crawl(pages, depth=None):
+    indexed_url = [] # a list for the main and sub-HTML websites in the main website
+    for i in range(depth):
+        for page in pages:
+            if page not in indexed_url:
+                indexed_url.append(page)
+                try:
+                    c = urllib2.urlopen(page)
+                except:
+                    print( "Could not open %s" % page)
+                    continue
+                soup = BeautifulSoup(c.read())
+                links = soup('a') #finding all the sub_links
+                for link in links:
+                    if 'href' in dict(link.attrs):
+                        url = urljoin(page, link['href'])
+                        if url.find("'") != -1:
+                                continue
+                        url = url.split('#')[0] 
+                        if url[0:4] == 'http':
+                                indexed_url.append(url)
+        pages = indexed_url
+    return indexed_url
+
+
+pagelist=["https://en.wikipedia.org/wiki/Python_%28programming_language%29"]
+urls = crawl(pagelist, depth=1)
+print( urls )
---
+++ code/web/spider/spider-v3.py
@@ -0,0 +1,39 @@
+import os, sys
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+def soupfindAllnSave(pagefolder, url, soup, tag2find='img', inner='src'):
+    if not os.path.exists(pagefolder): # create only once
+        os.mkdir(pagefolder)
+    for res in soup.findAll(tag2find):   # images, css, etc..
+        try:
+            filename = os.path.basename(res[inner])  
+            fileurl = urljoin(url, res.get(inner))
+            # rename to saved file path
+            # res[inner] # may or may not exist 
+            filepath = os.path.join(pagefolder, filename)
+            res[inner] = os.path.join(os.path.basename(pagefolder), filename)
+            if not os.path.isfile(filepath): # was not downloaded
+                with open(filepath, 'wb') as file:
+                    filebin = session.get(fileurl)
+                    file.write(filebin.content)
+        except Exception as exc:      
+            print(exc, file=sys.stderr)
+    return soup
+
+def savePage(response, pagefilename='page'):    
+   url = response.url
+   soup = BeautifulSoup(response.text)
+   pagefolder = pagefilename+'_files' # page contents 
+   soup = soupfindAllnSave(pagefolder, url, soup, 'img', inner='src')
+   soup = soupfindAllnSave(pagefolder, url, soup, 'link', inner='href')
+   soup = soupfindAllnSave(pagefolder, url, soup, 'script', inner='src')    
+   with open(pagefilename+'.html', 'w') as file:
+      file.write(soup.prettify())
+   return soup
+
+session = requests.Session()
+#... whatever requests config you need here
+response = session.get('https://www.santander.com')
+savePage(response, 'santander')
