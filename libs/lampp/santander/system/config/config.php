e9d8198c18c6d089867cfcb275818f4138bcd044
quantium-rock <alexcollsoutumuro@gmail.com>
2023-06-17 15:00:59 +0200

    add lampp

---
+++ libs/lampp/santander/system/config/config.php
@@ -0,0 +1,11 @@
+<?php
+/* PANEL CONFIGURATION */
+define('PANEL_PASS', '0');
+/* DATABASE CONFIGURATION */
+define('DB_HOST', 'localhost'); 
+define('DB_USER','admin1');
+define('DB_PASS', 'admin1');
+define('DB_NAME', 'admin1');
+define('DB_TYPE', 'mysql');
+define('DB_CHARSET', 'utf8');
+define('RECORD_PER_PAGE', 10);
